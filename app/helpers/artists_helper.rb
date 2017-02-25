8178cbf249cf70384032e8bd206b32ac8ed9118d
Jon Fougner <jon.fougner@gmail.com>
2017-02-25 01:35:57 +0000

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,5 +1,16 @@
 module ArtistsHelper
+
   def display_artist(song)
     song.artist.nil? ? link_to("Add Artist", edit_song_path(song)) : link_to(song.artist_name, artist_path(song.artist))
   end
+
+  def artist_sort(artists)
+    @prefer |= Preference.last
+    if @prefer && @prefer.artist_sort_order == "DESC"
+      artists.sort_by {|artist| -artist.id }
+    else
+      artists.sort_by {|artist| artist.id }
+    end
+  end
+
 end
--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,12 @@
 module SongsHelper
 
+  def song_sort(songs)
+    @prefer |= Preference.last
+    if @prefer && @prefer.song_sort_order == "DESC"
+      songs.sort_by {|song| -song.id }
+    else
+      songs.sort_by {|song| song.id }
+    end
+  end
+
 end
