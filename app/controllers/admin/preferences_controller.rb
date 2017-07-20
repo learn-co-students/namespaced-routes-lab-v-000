dc3bb70ae1a38bc8dd5ce727d7218f2dfcfdb6b1
benhayehudi <bengreenberg@gmail.com>
2017-07-20 16:07:50 -0400

    Done.

---
+++ app/controllers/admin/preferences_controller.rb
@@ -0,0 +1,13 @@
+class Admin::PreferencesController < ApplicationController
+
+  def index
+    @preference = Preference.first_or_create(song_sort_order: "ASC", artist_sort_order: "ASC", allow_create_artists: true, allow_create_songs: true)
+  end
+
+  private
+
+  def preference_params
+    params.require(:preference).permit(:artist_sort_order, :song_sort_order, :allow_create_artists, :allow_create_songs)
+  end
+
+end
