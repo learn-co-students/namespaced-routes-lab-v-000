4b067e95763ba0d29d083b3922d8f4fbe4ea359c
seanq <sean.r.quillen@gmail.com>
2017-08-01 15:27:44 -0500

    Done.

---
+++ app/controllers/admin/preferences_controller.rb
@@ -0,0 +1,13 @@
+class Admin::PreferencesController < ApplicationController
+
+	def index
+		@preference = Preference.first_or_create(allow_create_artists: true, allow_create_songs: true)	
+	end
+
+
+	def update
+		@preference = Preference.find(params[:id])
+		@preference.update(preference_params)
+		redirect_to admin_access_index_path
+	end
+end
