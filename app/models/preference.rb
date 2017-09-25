35d08e67c1de2ab2f2639926e115e28b3a0f7b89
leog7one <lguzman@me.com>
2017-09-25 16:33:11 -0500

    Done.

---
+++ app/models/preference.rb
@@ -0,0 +1,17 @@
+class Preference < ActiveRecord::Base
+
+	def self.artist_sort_order
+	end
+
+	def self.song_sort_order
+	end
+
+	def self.allow_create_artist
+	end
+
+	def self.allow_create_songs
+	end
+
+
+
+end
