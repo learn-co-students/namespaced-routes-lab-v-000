d796ccacccbd5c8df3237599573ce977cb128897
Nour Kodeih <nour.kodeih@gmail.com>
2018-01-09 18:22:43 +0000

    Done.

---
+++ app/models/preference.rb
@@ -0,0 +1,11 @@
+class Preference < ActiveRecord::Base
+  # attr_accessor :allow_create_artsits
+  # def allow_create_artsits=(value)
+  #   @allow_create_artsits = value
+  # end
+  #
+  # def allow_create_artsit
+  #   @allow_create_artsits
+  # end
+
+end
