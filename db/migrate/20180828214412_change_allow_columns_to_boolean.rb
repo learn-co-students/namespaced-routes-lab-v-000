b56d025924f251f5ec62f67b75b65d0426f7e5ad
Shirley Collins <shirley@m104.us>
2018-08-28 14:54:16 -0700

    Done.

---
+++ db/migrate/20180828214412_change_allow_columns_to_boolean.rb
@@ -0,0 +1,6 @@
+class ChangeAllowColumnsToBoolean < ActiveRecord::Migration
+  def change
+    change_column :preferences, :allow_create_artists, :boolean
+    change_column :preferences, :allow_create_songs, :boolean
+  end
+end
